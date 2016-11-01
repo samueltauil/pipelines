f9ddeb76e2d0f993e70f765f09e852271635c489
Samuel Tauil <samueltauil@gmail.com>
2016-11-01 00:53:10 -0400

    defining role to pull from dev

--- resources/setroles.sh
+++ resources/setroles.sh
@@ -5,6 +5,7 @@ oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n dev
 oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n dev
 oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n qa
 oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n dev
 oc create deploymentconfig simpledemo --image=$registry/dev/simpledemo:promoteToQA -n qa
 oc expose dc simpledemo --port=8080 -n qa
 oc expose svc simpledemo -n qa
