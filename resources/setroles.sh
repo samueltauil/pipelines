742e2c542c5eb4527227a4fb8ad09701de490d9e
Samuel Tauil <samueltauil@sardinha.local>
2017-04-04 01:23:57 -0400

    removing binaries

--- resources/setroles.sh
+++ resources/setroles.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 registry=$1
-oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n dev
-oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n dev
-oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n qa
+oc policy add-role-to-user edit system:serviceaccount:ci:jenkins -n dev
+oc policy add-role-to-group system:image-puller system:serviceaccounts:ci -n dev
+oc policy add-role-to-user edit system:serviceaccount:ci:jenkins -n qa
 # all service accounts on ci-cd project to pull images from qa
-oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:ci -n qa
 # all service accounts on qa project to pull images from dev
 oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n dev
 
 oc create deploymentconfig simpledemo --image=$registry/dev/simpledemo:promoteToQA -n qa
+oc deploy dc/simpledemo --cancel
 oc expose dc simpledemo --port=8080 -n qa
 oc expose svc simpledemo -n qa
